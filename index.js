3a6cd45c7e5292d1b98c6d8eec8a46beb5194a1d
rubens-juniorr <rubensjunior_20@icloud.com>
2024-04-11 13:27:50 -0300

    Modifcação do Java script

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 const cep = document.querySelector("#cep");
-const latitude = document.querySelector("#lat");
-const longitude = document.querySelector("#lnog");
 
-const tempo = document.getElementById("inputlg");
 const ceptabela = document.getElementById("cepTB");
 const bairro = document.getElementById("bairroTB");
 const cidade = document.getElementById("cityTB");
 
 async function getAddByendereco() {
-    
- const apiURL = `https://viacep.com.br/ws/${cep.value}/json/`;
+  const apiURL = `https://viacep.com.br/ws/${cep.value}/json/`;
 
+  const response = await fetch(apiURL);
 
-const response = await fetch(apiURL); 
+  const data = await response.json();
+  console.log(data);
 
-const data = await response.json();
-console.log(data);
+  getTempo();
 
+  ceptabela.innerHTML = data.logradouro;
+  bairro.innerHTML = data.bairro;
+  cidade.innerHTML = `${data.localidade} / ${data.uf}`;
+}
+async function getTempo() {
+  const lat = document.querySelector("#latitude");
+  const long = document.querySelector("#longitde");
+  const tempo = document.getElementById("inputlg");
 
-ceptabela.innerHTML = data.logradouro;
-bairro.innerHTML = data.bairro;
-cidade.innerHTML =  `${data.localidade} / ${data.uf}`;
+  
+  const apitempo = `https://api.open-meteo.com/v1/forecast?latitude=${lat.value}&longitude=${long.value}&hourly=temperature_2m`;
 
+  const res = await fetch(apitempo);
+  const prevTemp = await res.json();
 
-async function getTempo(){
- const apitempo = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`;
+  console.log(prevTemp);
 
- const res = await fetch(apitempo)
- const tempo = await res.json();
- 
-
-console.log(tempo);
-
-};
-
-
-};
-
-
-
+  tempo.value = prevTemp.temperature_2m;
+}
