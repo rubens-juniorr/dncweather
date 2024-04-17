7bd3140be43085796fd21398309bf4dfc350f642
rubens-juniorr <rubensjunior_20@icloud.com>
2024-04-17 16:39:38 -0300

    busca do grau

--- index.js
+++ index.js
@@ -12,40 +12,28 @@ async function getAddByendereco() {
   const data = await response.json();
   console.log(data);
 
-  getTempo();
+  getPrevisao();
 
   ceptabela.innerHTML = data.logradouro;
   bairro.innerHTML = data.bairro;
   cidade.innerHTML = `${data.localidade} / ${data.uf}`;
 }
-async function getTempo(dateString) {
-  const lat = document.getElementById("latitude");
-  const long = document.getElementById("longitde");
-
+async function getPrevisao() {
+  const lat = document.getElementById("latitude").value;
+  const long = document.getElementById("longitude").value;
   try {
-    const apitempo = `https://api.open-meteo.com/v1/forecast?latitude=${lat.value}&longitude=${long.value}&hourly=temperature_2m`;
-
-    const res = await fetch(apitempo);
-    const prevTemp = await res.json();
-
-    console.log(prevTemp);
-       const tempo = document.getElementById("inputlg").innerHTML = '';
-    /*for (
-      let index = 0;
-      index < prevTemp.hourly.temperature_2m.length;
-      index++
-    ) {
-    }*/
-      const data = new Date();
-      const hora = data.getHours();
-      console.log(hora);
-      console.log(typeof( prevTemp.hourly.time[0]));
-      console.log(Date.parse("2024-04-19T04:00"));
-
-     //Date.parse("2024-04-19T04:00");
-
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + lat +
+        '&longitude=' +
+        long +
+        '&hourly=temperature_2m'
+    );
+    const data = await response.json();
+    console.log(data);
+    document.getElementById('previsao').innerHTML =
+       'Previsão de tempo de acordo com a região: '  +
+      data.hourly.temperature_2m[12] +
+     '° C';
   } catch (error) {
-    alert(error.message);
-  };
-
-};
+    alert("Latitude e Longitudes inseridos invalidos.");
+  }
+}
