91920b4324cfcacdd557a7940054ee51f364a48b
rubens-juniorr <rubensjunior_20@icloud.com>
2024-04-24 09:35:26 -0300

    Final

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const cep = document.querySelector("#cep");
-
 const ceptabela = document.getElementById("cepTB");
 const bairro = document.getElementById("bairroTB");
 const cidade = document.getElementById("cityTB");
@@ -22,18 +21,23 @@ async function getPrevisao() {
   const lat = document.getElementById("latitude").value;
   const long = document.getElementById("longitude").value;
   try {
-    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + lat +
-        '&longitude=' +
+    const response = await fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=" +
+        lat +
+        "&longitude=" +
         long +
-        '&hourly=temperature_2m'
+        "&hourly=temperature_2m"
     );
     const data = await response.json();
     console.log(data);
-    document.getElementById('previsao').innerHTML =
-       'Previsão de tempo de acordo com a região: '  +
+    document.getElementById("previsao").value =
+      "Previsão de tempo de acordo com a região: " +
       data.hourly.temperature_2m[12] +
-     '° C';
+      "°C";
   } catch (error) {
     alert("Latitude e Longitudes inseridos invalidos.");
   }
 }
+
+//52.52
+//13.419998
